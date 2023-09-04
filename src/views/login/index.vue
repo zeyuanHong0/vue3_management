577edbc95b66a082f4fec0b3d239aa9b6c60171e
zeyuanHong <2029886425@qq.com>
2023-09-04 15:28:42 +0800

    添加图片资源

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,6 +8,9 @@ export default { name: 'login' }
 
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue'
+
+const username = ref('')
+const password = ref('')
 </script>
 <style lang="scss" scoped>
 @import './scss/index.scss';
