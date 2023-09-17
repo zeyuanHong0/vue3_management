42ef0305ebf4ee9ef80c0a7622e2de974fdbe9b4
zeyuanHong <2029886425@qq.com>
2023-09-17 14:56:05 +0800

    完成登录模块的静态页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="login_container"></div>
+  <div class="login_container">
+    <el-row>
+      <!-- 左侧(占位) -->
+      <el-col :span="12" :xs="0"></el-col>
+      <!-- 右侧 -->
+      <el-col :span="12" :xs="24">
+        <!-- 表单 -->
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到哲理源甄选</h2>
+          <!-- 用户名 -->
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="username"
+              placeholder="请输入用户名"
+            ></el-input>
+          </el-form-item>
+          <!-- 密码 -->
+          <el-form-item>
+            <el-input
+              :prefix-icon="Lock"
+              v-model="password"
+              placeholder="请输入密码"
+              type="password"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <!-- 登录按钮 -->
+          <el-form-item>
+            <el-button class="login_button" type="primary">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +43,7 @@ export default { name: 'login' }
 
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
 
 const username = ref('')
 const password = ref('')
