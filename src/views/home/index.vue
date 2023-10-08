6767028d981f0ee8b6c2866747b6bfbb1004fea4
zeyuanHong <2029886425@qq.com>
2023-10-08 11:19:05 +0800

    完成左侧菜单静态搭建

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,30 @@
     <div class="layout_menu">
       <!-- logo -->
       <logo />
+      <!-- 菜单内容 -->
+      <el-scrollbar class="scrollbar">
+        <el-menu
+          :default-active="activeIndex"
+          class="my_el_menu"
+          background-color="#001529"
+          text-color="white"
+          mode="vertical"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <template #title>
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部 haeder -->
     <div class="layout_tabbar">顶部</div>
@@ -18,6 +42,7 @@ export default { name: 'home' }
 
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue'
+
 import logo from './logo/index.vue'
 </script>
 <style lang="scss" scoped>
